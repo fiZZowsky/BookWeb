e12399dc80129d649c8a87d0386b1d17c7cf766d
fiZZowsky <piotrekr53@interia.pl>
2023-03-19 23:30:59 +0100

    added login page

---
+++ BookWeb/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using BookWeb.Data;
+using BookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LoginController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<User> objUsersList = _db.users;
+            return View(objUsersList);
+        }
+    }
+}
