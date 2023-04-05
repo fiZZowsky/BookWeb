eefec1d41036645da41dc4e3d0174e43c881841d
fiZZowsky <piotrekr53@interia.pl>
2023-04-06 00:16:40 +0200

    improved responsiveness of the home page and made some minor improvements to the appearance of the navigation bar

--- BookWeb/Controllers/BooksController.cs
+++ BookWeb/Controllers/BooksController.cs
@@ -3,6 +3,8 @@ using BookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.Reflection;
+using static System.Reflection.Metadata.BlobBuilder;
 
 public class BooksController : Controller
 {
@@ -28,4 +30,37 @@ public class BooksController : Controller
 
         return View(model);
     }
+
+    //public IActionResult Index(int? categoryId, string sortOrder)
+    //{
+    //    var categories = _context.Categories.ToList();
+    //    var books = _context.Books.Include(b => b.Category).ToList();
+    //    var authors = _context.Authors.ToList();
+
+    //    if (categoryId.HasValue)
+    //    {
+    //        ViewBag.TitleSortParam = String.IsNullOrEmpty(sortOrder) ? "Title A-Z" : "";
+    //        ViewBag.AddDateSortParam = sortOrder == "releaseDate" ? "Release Date" : "";
+    //        switch (sortOrder)
+    //        {
+    //            case "Title A-Z":
+    //                books = books.OrderBy(b => b.Title).Where(b => b.CategoryId == categoryId.Value).ToList();
+    //                break;
+    //            case "Title Z-A":
+    //                books = books.OrderByDescending(b => b.Title).Where(b => b.CategoryId == categoryId.Value).ToList();
+    //                break;
+    //            case "Release Date":
+    //                books = books.OrderBy(b => b.ReleaseDate).Where(b => b.CategoryId == categoryId.Value).ToList();
+    //                break;
+    //            default:
+    //                books = books.OrderBy(b => b.Title).ToList();
+    //                break;
+    //        }
+
+
+    //        var model = new Tuple<IEnumerable<Category>, IEnumerable<Book>>(categories, books);
+
+    //        return View(model);
+    //    }
+    //}
 }
